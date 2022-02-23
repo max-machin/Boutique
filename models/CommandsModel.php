bc38d0f4fa048a3a74fa629c56869cf4fe802d0e
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-02-23 14:43:00 +0100

    getter et setter de categorie

--- models/CommandsModel.php
+++ models/CommandsModel.php
@@ -4,8 +4,38 @@ class CommandsModel extends Model
 {
     protected $id;
     protected $name;
-    protected $description;
-    protected $price;
-    protected $id_categories;
-    protected $id_sous_categories;
+    // protected $description;
+    // protected $price;
+    // protected $id_categories;
+    // protected $id_sous_categories;
+
+    public function __construct()
+    {
+        $this->table = "categories";
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
 }
