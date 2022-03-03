ccb505054fd80b5e7ac0cfcbc05e8b92a854569d
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-03 11:29:49 +0100

    changement dans le feature

--- views/layout.html.php
+++ views/layout.html.php
@@ -18,7 +18,6 @@
 
                     <?php
                 // balise php avec la condition de reconnaisance du profil user
-<<<<<<< HEAD
                     // echo('<li class="navli"><a href="index.php">Home</a></li>');
                     // echo ('<li class="navli"><a href="users">Connexion</a></li>');
                     // echo ('<li class="navli"><a href="users">Inscription</a></li>');
@@ -28,7 +27,6 @@
                     // {
                     //     echo ('<li class="navli"><a href="'. url.'products/'.$categorie[0]['name'].'">'.$categorie['name'].'</a></li>');
                     // }
-=======
               
                     echo('<li class="navli"><a href="index.php">Home</a></li>');
                     echo ('<li class="navli"><a href="users">Connexion</a></li>');
@@ -37,7 +35,6 @@
                     {
                         echo ('<li class="navli"><a href="'.urlmac.'products/'.$categorie['name'].'">'.$categorie['name'].'</a></li>');
                     }
->>>>>>> product
                 
                 ?>
             </ul>
