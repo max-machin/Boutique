82b4cdc7a57ff465257f91a11e5a9a40bd1dcd17
max-machin <machin.max@lapalteforme.io>
2022-03-11 22:55:26 +0100

    Style css des formulaire du profil, début du style des recap commande dans profil user

--- views/users/profil.html.php
+++ views/users/profil.html.php
@@ -4,39 +4,51 @@ if ( empty ( $user)) {
     header('location: login');
 } else {
 ?>
-    <h2>Vos informations personnelles</h2>
+    <h2 class="sous-titre">Vos informations personnelles</h2>
     <form action="" method="post">
-        <label for="email">E-mail * (facturation)</label></br>
-        <input id="email" type="email" name="email" value="<?= $user['email'] ?>"></br>
-
-        <label for="surname">Prénom *</label></br>
-        <input id="surname" type="text" name="surname" value="<?= $user['prenom'] ?>"></br>
-
-        <label for="name">Nom *</label></br>
-        <input id="name" type="text" name="name" value="<?= $user['nom'] ?>"></br>
         
-        <input type="submit" name="submit" value="Modifier infos">
+        <div class="form-group">
+            <input id="email" type="email" name="email" value="<?= $user['email'] ?>" required>
+            <label for="email">E-mail * (facturation)</label>
+        </div>
+        <div class="form-group">
+            <input id="surname" type="text" name="surname" value="<?= $user['prenom'] ?>" required>
+            <label for="surname">Prénom *</label>
+        </div>
+        <div class="form-group">
+            <input id="name" type="text" name="name" value="<?= $user['nom'] ?>" required>
+            <label for="name">Nom *</label>
+        </div>
+        <input class="submit submit-profil" type="submit" name="submit" value="Modifier infos">
     </form>
 
+    
     <form action="" method="post" style="display: <?= $display1 ?>">
-        <label for="oldPassword">Ancien mot de passe *</label></br>
-        <input id="oldPassword" type="password" name="oldPassword" placeholder="******"></br>
-        <span><?= $error_old_password ?></span></br>
-        <input type="submit" name="subPassword" value="Confirmer password">
+    <h3>Mot de passe</h3>
+        <div class="form-group">
+            <input id="oldPassword" type="password" name="oldPassword" required>
+            <label for="oldPassword">Ancien mot de passe *</label>
+            <span><?= $error_old_password ?></span>
+        </div>
+        <input class="submit submit-profil" type="submit" name="subPassword" value="Confirmer password">
     </form>
 
     <form action="" method="post" style="display: <?= $display2 ?>">
-        <label for="newPassword">Nouveau mot de passe *</label></br>
-        <input id="newPassword" type="password" name="newPassword" placeholder="******"></br>
-        <span><?= $error_new_password ?></span></br>
-
-        <label for="validPassword">Valider mot de passe *</label></br>
-        <input id="validPassword" type="password" name="validPassword" placeholder="Confirmer mot de passe"></br>
-        <span><?= $error_validPassword ?></span></br>
-        <input type="submit" name="subNewPassword" value="Modifier password">
+    <h3>Mot de passe</h3>
+        <div class="form-group">
+            <input id="newPassword" type="password" name="newPassword" required>
+            <label for="newPassword">Nouveau mot de passe *</label>
+            <span><?= $error_new_password ?></span>
+        </div>
+        <div class="form-group">
+            <input id="validPassword" type="password" name="validPassword" required>
+            <label for="validPassword">Valider mot de passe *</label>
+            <span><?= $error_validPassword ?></span>
+        </div>
+        <input class="submit submit-profil" type="submit" name="subNewPassword" value="Modifier password">
     </form>
 
-    <h2>Vos dernières commandes</h2>*
+    <h2 class="sous-titre">Vos dernières commandes</h2>*
 
 <?php 
 
