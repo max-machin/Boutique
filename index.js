8949f01fe2eafd0ca8b2af626fa3c0770ecc71a5
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-03-31 11:44:55 +0200

    seeproduct view css js merged

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// window.addEventListener('load', function(){
-//     alert('hey');
-// }
-// );
 document.addEventListener('DOMContentLoaded', function loaded() {
 
             //NAV BURGER
@@ -47,6 +43,49 @@ document.addEventListener('DOMContentLoaded', function loaded() {
         form.classList.toggle("open");
     });
 
+
+    //pour le slider dans seeproduct
+
+    let img_slider = document.getElementsByClassName('img_slider');
+    
+    function definirFirstImg (){
+        img_slider[0].classList.add('active');
+    }
+    definirFirstImg();
+
+    let etape = 0;
+
+    let nb_img = img_slider.length;
+
+    let precedent = document.querySelector('#previous');
+    let suivant = document.querySelector('#next');
+
+    function enleverActiveImages() {
+        for(let i = 0; i < nb_img; i++){
+            img_slider[i].classList.remove('active');
+        }
+    }
+
+    suivant.addEventListener('click', function(){
+        etape++;
+        if(etape >= nb_img){
+            etape = 0;
+        }
+        enleverActiveImages();
+        // console.log(img_slider[etape]);
+        img_slider[etape].classList.add('active');
+    })
+
+    precedent.addEventListener('click', function(){
+        etape--;
+
+        if(etape < 0){
+            etape = nb_img - 1;
+        }
+
+        enleverActiveImages();
+        img_slider[etape].classList.add('active');
+    })
 
 //pour le pop up à l'accueil
    var close = document.getElementById('close');
