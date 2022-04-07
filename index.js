a770917b50de4de2e023853763f99ed4c6f51a5a
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-04-07 14:43:41 +0200

    instafeed rajout

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ document.addEventListener('DOMContentLoaded', function loaded() {
     });
 
 
-
 buttonNav.addEventListener("click", function(){
     //après avoir récupéré l'image qui m'intéresse plus haut, je lui dis que si je clique dessus, l'image change sinon ça revient à son image initiale
     //en plus de rajouter avec le toggle(propriété qui rajoute et retire) une classe css .open située à ma nav -> c'est cette classe qui a le transform 0% et donc permet de voir le menu
@@ -78,4 +77,53 @@ buttonNav.addEventListener("click", function(){
 }); 
 
 
-});
+    //INSTAFEED
+
+    const accessToken = "IGQVJXZAzljNlV0OHVUNDRUZAkNaZA28tRE5Nc09WMGp2Q3V3ekRTRjV2empnZA1kwSDc0UUE1WWlMbnppYnF0elFRNWFSZAFVyeFZAiWkgxZAnNEdEFSM3VwWlA3VFdoQkNOc2hrVXFZAMnBlTW9Dci1ZARlhXTQZDZD";
+
+    const fields = "id,media_type,media_url,timestamp,permalink";
+
+    const accessUrl = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${accessToken}`;
+
+    const section = document.getElementsByClassName("instafeed")
+    const instafeed = section[0];
+    
+    const fetchPosts = async () => {
+
+        try {
+
+            const response = await fetch(accessUrl)
+            const {data} = await response.json()
+
+            data.forEach(post => {
+                let a = document.createElement("a");
+                a.href = post.permalink
+                a.target = "_blank"
+                a.rel = "noreferrer noopener"
+
+                if(post.media_type === "VIDEO"){
+                    let video = document.createElement("video");
+                    video.src = post.media_url
+                    video.preload = true
+                    video.autoplay = true
+                    video.muted = true
+                    video.loop = true
+                    a.appendChild(video)
+                } else {
+                    let img = document.createElement("img");
+                    img.src = post.media_url
+                    a.appendChild(img)
+                }
+                
+                instafeed.appendChild(a)
+
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    fetchPosts();
+})
+
